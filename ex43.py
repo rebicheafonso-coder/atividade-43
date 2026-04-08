4acd99dda5d92a9366b34c64480b17bdc9a2b16c
rebicheafonso-coder <rebiche.afonso@escola.pr.gov.br>
2026-04-07 22:22:15 -0300

    Add files via upload

---
+++ ex43.py
@@ -0,0 +1,21 @@
+"def" ("menu_simples")
+print(Menu)
+print ("1 - consertar")
+print("2 - vender")
+print("3 - Comprar")
+print("4 - sair")
+
+pedido = int("imput")("Informe o numero da opção desejada")
+             
+
+             if pedido == "1":
+             print ("vocês escolheu consertar")
+
+             elif pedido == "2":
+             print("vocês escolheu vender")
+
+             elif pedido =="3":
+             print("Você escolheu comprar"
+             ""
+             "else:"
+             "print("opção sair)")
